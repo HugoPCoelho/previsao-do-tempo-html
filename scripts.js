9484f25324e8b05be34550665aa8b06234d9be72
HugoPCoelho <hugo.penha.coelho@gmail.com>
2023-12-26 19:13:06 -0300

    Tratamento de quando a cidade não existe

--- scripts.js
+++ scripts.js
@@ -2,20 +2,28 @@
 
 const key = "8b6f231d051c7841bed2b4936a93d542"
 
-function DisplayDataOnScreen(data) {
+function displayDataOnScreen(data) {
     document.querySelector(".cidade").innerHTML = "Tempo em " + data.name
     document.querySelector(".temp").innerHTML = Math.floor(data.main.temp) + "˚C"
     document.querySelector(".forecast").innerHTML = data.weather[0].description
     document.querySelector(".humidity").innerHTML = "Umidade: " + data.main.humidity + "%"
     document.querySelector(".img-forecast").src = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
-    
-    console.log(data)
+}
+
+function displayNotFound() {
+    document.querySelector(".cidade").innerHTML = "Cidade não localizada!"
+    document.querySelector(".temp").innerHTML = "0˚C"
+    document.querySelector(".forecast").innerHTML = ""
+    document.querySelector(".humidity").innerHTML = "Umidade: "
 }
 
 async function findCidade(cidade) {
     const data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=${key}&lang=pt_br&units=metric`).then(response => response.json())
     
-    DisplayDataOnScreen(data)
+    if(data.cod == 200)
+        displayDataOnScreen(data)
+    else
+        displayNotFound()
 }
 
 function buttonClick() {
